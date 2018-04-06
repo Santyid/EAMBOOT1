54d9721aa7fd89c7b680b4a6b7ac46a71fa113bb
Santyid <santiago-2108@hotmail.com>
2018-04-05 23:20:21 -0500

    Subida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'myApplication ';
+  autor = 'Santiago Idarraga';
+  count = 0;
+  imgSource = './../favicon.ico';
+  estado =  false ;
 }
